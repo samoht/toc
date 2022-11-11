002639f288063898c8acba881404696c7ae574eb
Thomas Gazagnaire <thomas@gazagnaire.org>
2022-11-11 19:38:05 +0100

    Add links in the TOC

--- lib/toc.ml
+++ lib/toc.ml
@@ -15,6 +15,7 @@
  *)
 
 open Omd
+open Astring
 
 let src = Logs.Src.create "irmin.tree" ~doc:"Persistent lazy trees for Irmin"
 
@@ -60,16 +61,35 @@ and skip_to_end ~toc : doc -> doc = function
       | _ -> skip_to_end ~toc t)
   | _ :: t -> skip_to_end ~toc t
 
+module Id = struct
+  (* Convert section title to a valid HTML ID. *)
+  let title_to_id s =
+    String.filter (fun c -> Char.Ascii.is_alphanum c || c = ' ') s
+    |> String.map (function ' ' -> '-' | c -> c)
+
+  let inline : 'attr inline -> 'attr inline =
+   fun label ->
+    let id = Pp_markdown.to_string [ Paragraph ([], label) ] in
+    Link ([], { label; destination = "#" ^ title_to_id id; title = None })
+
+  let rec block : 'attr block -> 'attr block = function
+    | Paragraph (attr, x) -> Paragraph (attr, inline x)
+    | List (attr, ty, sp, bl) ->
+        List (attr, ty, sp, List.map (List.map block) bl)
+    | _ -> failwith "invalid mardkown in TOC"
+end
+
 let toc ?depth doc =
   match Omd.toc ?depth ~start:[ 1 ] doc with
   | [] -> None
-  | [ toc ] -> Some toc
+  | [ toc ] -> Some (Id.block toc)
   | _ -> assert false (* this is an invariant in Omd.toc *)
 
 let expand ?depth doc =
   match toc ?depth doc with
   | None -> None
   | Some toc ->
+      Log.info (fun l -> l "TOC=%a" pp [ toc ]);
       Log.debug (fun l -> l "BEFORE: %a" pp doc);
       let doc = replace ~toc doc in
       Log.debug (fun l -> l "AFTER: %a" pp doc);
